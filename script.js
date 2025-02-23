a5dc4eca23625b50eba629ac0c03ab3456727979
Pittipat1 <marketsing099@gmail.com>
2025-02-23 19:56:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let openLoignRight = document.querySelector(".h1");
+let loginmain = document.querySelector(".login-main");
+
+openLoignRight;addEventListener('click', function() {
+    loginmain.classList.toggle('open');
+});
